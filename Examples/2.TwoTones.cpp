74ca84e67bedfe8fc0924c103de0dbe3774d8723
Joan Andrés <andres6936@live.com>
2020-02-10 17:18:58 -0500

    Fixed Example

--- Examples/2.TwoTones.cpp
+++ Examples/2.TwoTones.cpp
@@ -1,14 +1,10 @@
 //This examples shows another fundamental building block of digital audio - adding two sine waves together. When you add waves together they create a new wave whose amplitude at any time is computed by adding the current amplitudes of each wave together. So, if one wave has an amplitude of 1, and the other has an amplitude of 1, the new wave will be equal to 2 at that point in time. Whereas, later, if one wave has an amplitude of -1, and the other has an amplitude of 1, the new wave - the one you hear - will equal 0. This can create some interesting effects, including 'beating', when the waves interact to create a single wave that fades up and down based on the frequencies of the two interacting waves. The frequency of the 'beating' i.e. the fading in and out, is equal to the difference in frequency between the two waves.
 
 #include "Maximilian.hpp"
-#include "Realtime/Audio.hpp"
 
-Maximilian::Oscilation mySine, myOtherSine;//Two oscillators with names.
+using namespace Maximilian;
 
-void setup()
-{//some inits
-	//nothing to go here this time
-}
+Oscilation mySine, myOtherSine;//Two oscillators with names.
 
 void play(double* output)
 {//this is where the magic happens. Very slow magic.
@@ -21,7 +17,7 @@ void play(double* output)
 }
 
 int routing(void* outputBuffer, void* inputBuffer, unsigned int nBufferFrames,
-		double streamTime, Maximilian::RtAudioStreamStatus status, void* userData)
+		double streamTime, RtAudioStreamStatus status, void* userData)
 {
 
 	unsigned int i, j;
@@ -39,7 +35,7 @@ int routing(void* outputBuffer, void* inputBuffer, unsigned int nBufferFrames,
 	for (i = 0; i < nBufferFrames; i++)
 	{
 		play(lastValues);
-		for (j = 0; j < Maximilian::Settings::CHANNELS; j++)
+		for (j = 0; j < Settings::CHANNELS; j++)
 		{
 			*buffer++ = lastValues[j];
 		}
@@ -49,32 +45,24 @@ int routing(void* outputBuffer, void* inputBuffer, unsigned int nBufferFrames,
 
 int main()
 {
-	setup();
+	Audio audio(Audio::SupportedArchitectures::Windows_Ds);
 
-	Maximilian::Audio dac(Maximilian::Audio::SupportedArchitectures::Windows_Ds);
-	if (dac.getDeviceCount() < 1)
-	{
-		std::cout << "\nNo audio devices found!\n";
-		char input;
-		std::cin.get(input);
-		exit(0);
-	}
 
-	Maximilian::Audio::StreamParameters parameters;
-	parameters.deviceId = dac.getDefaultOutputDevice();
-	parameters.nChannels = Maximilian::Settings::CHANNELS;
+	Audio::StreamParameters parameters;
+	parameters.deviceId = audio.getDefaultOutputDevice();
+	parameters.nChannels = Settings::CHANNELS;
 	parameters.firstChannel = 0;
-	unsigned int sampleRate = Maximilian::Settings::SAMPLE_RATE;
-	unsigned int bufferFrames = Maximilian::Settings::BUFFER_SIZE;
+	unsigned int sampleRate = Settings::SAMPLE_RATE;
+	unsigned int bufferFrames = Settings::BUFFER_SIZE;
 	//double data[maxiSettings::channels];
-	vector <double> data(Maximilian::Settings::CHANNELS, 0);
+	vector <double> data(Settings::CHANNELS, 0);
 
 	try
 	{
-		dac.openStream(parameters, RTAUDIO_FLOAT64,
+		audio.openStream(parameters, RTAUDIO_FLOAT64,
 				sampleRate, &bufferFrames, &routing, (void*)&(data[0]));
 
-		dac.startStream();
+		audio.startStream();
 	}
 	catch (Exception& e)
 	{
@@ -89,15 +77,15 @@ int main()
 	try
 	{
 		// Stop the stream
-		dac.stopStream();
+		audio.stopStream();
 	}
 	catch (Exception& e)
 	{
 		e.printMessage();
 	}
 
-	if (dac.isStreamOpen())
-	{ dac.closeStream(); }
+	if (audio.isStreamOpen())
+	{ audio.closeStream(); }
 
 	return 0;
 }
