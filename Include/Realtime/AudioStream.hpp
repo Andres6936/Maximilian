0d162e6c1e3ce24e32a5caa04440c42b9d165766
Joan Andrés <andres6936@live.com>
2020-02-12 14:06:37 -0500

    Use of std::array instead of array in raw

--- Include/Realtime/AudioStream.hpp
+++ Include/Realtime/AudioStream.hpp
@@ -5,6 +5,8 @@
 #include "CallbackInfo.hpp"
 #include "Definition/AudioFormat.hpp"
 
+#include <array>
+
 namespace Maximilian
 {
 
@@ -39,30 +41,67 @@ namespace Maximilian
 		char* userBuffer[2] = { nullptr, nullptr };       // Playback and record, respectively.
 		char* deviceBuffer = nullptr;
 
-		bool doConvertBuffer[2] = { false, false };   // Playback and record, respectively.
 		bool userInterleaved = true;
-		bool deviceInterleaved[2] = { true, true }; // Playback and record, respectively.
-		bool doByteSwap[2] = { false, false };        // Playback and record, respectively.
 
-		unsigned int device[2] = { 11'111, 11'111 };    // Playback and record, respectively.
+		unsigned int nBuffers = 0;
 		unsigned int sampleRate = 0;
 		unsigned int bufferSize = 0;
-		unsigned int nBuffers = 0;
-		unsigned int nUserChannels[2] = { 0, 0 };    // Playback and record, respectively.
-		unsigned int nDeviceChannels[2] = { 0, 0 };  // Playback and record channels, respectively.
-		unsigned int channelOffset[2] = { 0, 0 };    // Playback and record, respectively.
-
-		unsigned long latency[2] = { 0, 0 };         // Playback and record, respectively.
 
 		double streamTime = 0.0;         // Number of elapsed seconds since the stream started.
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<bool, 2> doConvertBuffer = { false, false };
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<bool, 2> deviceInterleaved = { true, true };
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<bool, 2> doByteSwap = { false, false };
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<unsigned int, 2> nUserChannels = { 0, 0 };
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<unsigned int, 2> nDeviceChannels = { 0, 0 };
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<unsigned int, 2> channelOffset = { 0, 0 };
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<unsigned int, 2> device = { 11'111, 11'111 };
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<unsigned long, 2> latency = { 0, 0 };
+
+		/**
+		 * Playback and record, respectively.
+		 */
+		std::array<AudioFormat, 2> deviceFormat = { AudioFormat::Float64,
+													AudioFormat::Float64 };
 
 		pthread_mutex_t mutex;
 
 		StreamMode mode = StreamMode::UNINITIALIZED;           // OUTPUT, INPUT, or DUPLEX.
+
 		StreamState state = StreamState::STREAM_CLOSED;         // STOPPED, RUNNING, or CLOSED
+
 		AudioFormat userFormat = AudioFormat::Float64;
-		AudioFormat deviceFormat[2] = { AudioFormat::Float64,
-										AudioFormat::Float64 };    // Playback and record, respectively.
 		CallbackInfo callbackInfo;
 		ConvertInfo convertInfo[2] = { };
 
